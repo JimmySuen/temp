f44dc88bd53266c6245392d9970a586a7c99ef2e
xiao <sunxiaojay123@gmail.com>
2022-05-13 02:09:29 +0000

    minor

--- copy_file.sh
+++ copy_file.sh
@@ -1,3 +1,4 @@
 cp eval_cls.py ../ibot/evaluation/semi_supervised/
 cp utils.py ../ibot/
 cp -r zhirong/ ../ibot/
+cp create_imagenet_subsets.py ../ibot/ 
--- create_imagenet_subsets.py
+++ create_imagenet_subsets.py
@@ -2,13 +2,13 @@ import os
 import shutil
 from pathlib import Path
 
-src_folder = "/data/xias/data/ImageNet/"
+src_folder = "/mnt/blobfuse/data/ImageNet/"
 
-txt_file = "/data/xias/projects/ibot/imagenet_subsets/1percent.txt"
-# txt_file = "/data/xias/projects/ibot/imagenet_subsets/10percent.txt"
+txt_file = "/mnt/blobfuse/projects/simclr/imagenet_subsets/1percent.txt"
 
-dst_folder = "/data/xias/data/ImageNetS1/"
-# dst_folder = "/data/xias/data/ImageNetS10/"
+
+dst_folder = "/mnt/blobfuse/data/ImageNetS1/"
+
 
 with open(txt_file, 'r') as f:
     Lines = f.readlines()
@@ -23,3 +23,22 @@ with open(txt_file, 'r') as f:
         Path(dst).mkdir(parents=True, exist_ok=True)
         dst_file = os.path.join(dst, name)
         shutil.copyfile(src, dst_file)
+
+
+txt_file = "/mnt/blobfuse/projects/simclr/imagenet_subsets/10percent.txt"
+dst_folder = "/mnt/blobfuse/data/ImageNetS10/"
+
+with open(txt_file, 'r') as f:
+    Lines = f.readlines()
+    count = 0
+    for line in Lines:
+        count += 1
+        name = line.strip()
+        folder = name.split("_")[0]
+        print("Line{}: {}".format(count, name))
+        src = os.path.join(src_folder, 'train', folder, name)
+        dst = os.path.join(dst_folder, 'train', folder)
+        Path(dst).mkdir(parents=True, exist_ok=True)
+        dst_file = os.path.join(dst, name)
+        shutil.copyfile(src, dst_file)
+
