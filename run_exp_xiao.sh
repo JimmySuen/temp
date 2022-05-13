97776290393a05d706e803958f68be1e97247abc
xiao <sunxiaojay123@gmail.com>
2022-05-13 02:42:11 +0000

    minor

--- run_exp_xiao.sh
+++ run_exp_xiao.sh
@@ -1,10 +1,10 @@
-mkdir output_s1
+mkdir outputs1_v10_base_mask08_1crops_droppath01_wd01
 
-python -m torch.distributed.launch --nproc_per_node=8 evaluation/semi_supervised/eval_cls.py --pretrained_weights /data/home/xias/projects/ibot/pretrained_models/ViTS16/checkpoint.pth --avgpool_patchtokens 0 --arch vit_small --checkpoint_key teacher --output_dir output_s1/ --finetune_head_layer 1 --epochs 1000 --lr 5e-6 --data_path /mnt/blobfuse/data/ImageNetS1/
+python -m torch.distributed.launch --nproc_per_node=8 evaluation/semi_supervised/eval_cls.py --pretrained_weights /mnt/blobfuse/projects/ibot/pretrained_models/zhirong/v10_base_mask08_1crops_droppath01_wd01/current.pth.tar --avgpool_patchtokens 0 --arch vit_base --checkpoint_key teacher --output_dir outputs1_v10_base_mask08_1crops_droppath01_wd01/ --finetune_head_layer 1 --epochs 1000 --lr 5e-6 --data_path /mnt/blobfuse/data/ImageNetS1/
 
 
-mkdir output_s10
+mkdir outputs10_v10_base_mask08_1crops_droppath01_wd01
 
-python -m torch.distributed.launch --nproc_per_node=8 evaluation/semi_supervised/eval_cls.py --pretrained_weights /data/home/xias/projects/ibot/pretrained_models/ViTS16/checkpoint.pth --avgpool_patchtokens 0 --arch vit_small --checkpoint_key teacher --output_dir output_s10/ --finetune_head_layer 1 --epochs 1000 --lr 5e-6 --data_path /mnt/blobfuse/data/ImageNetS10/
+python -m torch.distributed.launch --nproc_per_node=8 evaluation/semi_supervised/eval_cls.py --pretrained_weights /mnt/blobfuse/projects/ibot/pretrained_models/zhirong/v10_base_mask08_1crops_droppath01_wd01/current.pth.tar --avgpool_patchtokens 0 --arch vit_base --checkpoint_key teacher --output_dir outputs10_v10_base_mask08_1crops_droppath01_wd01/ --finetune_head_layer 1 --epochs 1000 --lr 5e-6 --data_path /mnt/blobfuse/data/ImageNetS10/
 
 
